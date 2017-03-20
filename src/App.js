7996515d9411bebcb293ab0639e5a7c6e6c0937d
BrOrlandi <brorlandi@gmail.com>
2017-03-20 00:16:11 -0300

    WIP component and playground page.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
-import BigText from 'big-text.js';
+import BigText from './react-bigtext';
 
 class App extends Component {
 
-  componentDidMount() {
-    console.log(BigText);
-    BigText(this.refs.test,{
-      limitingDimension: "width"
-    });
+  constructor(props){
+    super(props);
+
+    this.state= {
+      children:"Big Text Component",
+      options: `{
+  "height" : 100
+}`
+    }
+  }
+
+  handleChangeChildren = (event)=>{
+    this.setState({children: event.target.value});
+  }
+
+  handleChangeOptions = (event)=>{
+    this.setState({options: event.target.value});
   }
 
   render() {
+    let options = {};
+    try {
+      options = JSON.parse(this.state.options);
+    } catch (error) {
+      
+    }
     return (
       <div className="App">
         <div className="App-header">
-          <h1>BigText React Component</h1>
+          <BigText options={{height: 50}}>BigText React Component</BigText>
         </div>
         <div className="App-content">
           <div className="App-inner">
-            <div className="textTest">
-              <span ref="test">Big Text Component</span>
-            </div>
+            <label htmlFor="content">Children Content:</label>
+            <br/>
+            <textarea name="content" id="content" ref="content" cols="30" rows="10" value={this.state.children} onChange={this.handleChangeChildren}></textarea>
+            <br/>
+            <label htmlFor="options">BigText options:</label>
+            <br/>
+            <textarea name="options" id="options" ref="options" cols="30" rows="10" value={this.state.options} onChange={this.handleChangeOptions}>
+            </textarea>
+            <br/>
+            <br/>
+            <BigText className="textTest" options={options}>
+              {this.state.children}
+            </BigText>
           </div>
         </div>
       </div>
---
+++ src/react-bigtext.js
@@ -0,0 +1,49 @@
+import React, { Component, PropTypes } from 'react';
+import bigtext from 'big-text.js';
+
+class BigText extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+        this._update();
+    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+
+    // }
+
+    componentDidUpdate(prevProps, prevState) {
+        this._update();
+    }
+    
+    _update(){
+        bigtext(this.refs.span,{...this.props.options});
+    }
+
+    render() {
+        let style = {};
+        let opts = this.props.options;
+        if(typeof opts.height !== "undefined"){
+            style.height = opts.height; 
+        }
+        if(typeof opts.width !== "undefined"){
+            style.width = opts.width; 
+        }
+        return (
+            <div {...this.props} style={style}>
+                <span ref="span">
+                    {this.props.children}
+                </span>
+            </div>
+        );
+    }
+}
+
+BigText.propTypes = {
+    options: PropTypes.object
+};
+
+export default BigText;
